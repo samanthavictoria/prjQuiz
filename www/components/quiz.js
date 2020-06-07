a8228ab213fc9740cf16613ce1ac2d6ecde4b414
samanthavictoria <sasavicsouza@gmail.com>
2020-06-08 06:32:51 +0900

    quase finalizado, falta a ultima pergunta e o css

--- www/components/quiz.js
+++ www/components/quiz.js
@@ -1,2 +1,45 @@
 // This is a JavaScript file
+$(document).on("click","#e1",function(){
+  function confirma(buttonIndex){
+    if(buttonIndex == "1"){
+      navigator.notification.alert("Sua resposta está correta!");
+      navigator.notification.beep(1);
+    }
+    else{
+       navigator.notification.alert("Sua resposta está errada :(");
+       navigator.vibrate(1500);
+    }
+   
+  }
+  navigator.notification.confirm("escolha A ou B",confirma,"Escolha:",['A','B']);
+});
 
+$(document).on("click","#e2",function(){
+  function confirma(buttonIndex){
+    if(buttonIndex == "1"){
+      navigator.notification.alert("Sua resposta está correta!");
+      navigator.notification.beep(1);
+    }
+    else{
+       navigator.notification.alert("Sua resposta está errada :(");
+       navigator.vibrate(1500);
+    }
+   
+  }
+  navigator.notification.confirm("escolha A ou B",confirma,"Escolha:",['A','B']);
+});
+
+$(document).on("click","#e3",function(){
+  function confirma(buttonIndex){
+    if(buttonIndex == "1"){
+      navigator.notification.alert("Sua resposta está correta!");
+      navigator.notification.beep(1);
+    }
+    else{
+       navigator.notification.alert("Sua resposta está errada :(");
+       navigator.vibrate(1500);
+    }
+   
+  }
+  navigator.notification.confirm("escolha A ou B",confirma,"Escolha:",['A','B']);
+});
